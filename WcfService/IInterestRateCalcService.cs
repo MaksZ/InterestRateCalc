9b531ddb0a9f14575069c96df0143ee9d0bc3758
Maksim Zavidejev <maksim.zavidejev@gmail.com>
2017-03-21 23:54:22 +0200

    service first step

---
+++ WcfService/IInterestRateCalcService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfService
+{
+    [ServiceContract]
+    public interface IInterestRateCalcService
+    {
+        [OperationContract]
+        SebCustomer GetCustomer(string personelId);
+
+        //[OperationContract]
+        //CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        // TODO: Add your service operations here
+    }
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    // You can add XSD files into the project. After building the project, you can directly use the data types defined there, with the namespace "WcfService.ContractType".
+    [DataContract]
+    public class SebCustomer
+    {
+        [DataMember]
+        public string PersonalId { get; set; }
+
+        [DataMember]
+        public string FirstName { get; set; }
+
+        [DataMember]
+        public string LastName { get; set; }
+    }
+}
---
+++ WcfService/InterestRateCalcService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class InterestRateCalcService : IInterestRateCalcService
+    {
+        public SebCustomer GetCustomer(string personelId)
+        {
+            using (var context = new InterestRateCalc.DAL.SebContext())
+            {
+                var customer = context.Customers.FirstOrDefault(x => x.PersonalId == personelId);
+
+                if (customer == null) return null;
+
+                var result = new SebCustomer
+                {
+                    PersonalId = customer.PersonalId,
+                    FirstName = customer.FirstName,
+                    LastName = customer.LastName
+                };
+
+                return result;
+            }
+        }
+    }
+}
